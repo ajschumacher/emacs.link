595b3c5b8333f4c469f68837d800ea301e92faea
Aaron Schumacher <ajschumacher@gmail.com>
2014-12-06 10:44:56 -0500

    split out to type-specific files

---
+++ script.js
@@ -0,0 +1,18 @@
+var html = document.getElementsByTagName('html')[0];
+var htmlStyle = window.getComputedStyle(html);
+var body = document.getElementsByTagName('body')[0];
+var bodyStyle = window.getComputedStyle(body);
+var niceSize = function () {
+    var width = parseInt(htmlStyle.getPropertyValue("width")
+                         .replace('px', ''));
+    var contentHeight = parseInt(bodyStyle.getPropertyValue("height")
+                                 .replace('px', ''));
+    var windowHeight = window.innerHeight;
+    // background image is 1704 wide by 2416 tall
+    // and tree part is about 876 tall
+    var treeHeight = Math.ceil(width * 876 / 1704);
+    var necessaryHeight = contentHeight + treeHeight;
+    html.style.minHeight = Math.max(windowHeight, necessaryHeight) + "px";
+};
+niceSize();
+window.addEventListener("resize", niceSize);
